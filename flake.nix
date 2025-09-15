73ab8e269ca66aaee3c6015ab6ebc0f200ff84f0
Priyanshu Prasad Gupta <priyanshoon.pg@gmail.com>
2025-09-15 09:05:16 +0530

    update: devenv setup

--- flake.nix
+++ flake.nix
@@ -2,10 +2,23 @@
     description = "Interpreter for fun";
 
     inputs = {
-
+        flake-utils.url = "github:numtide/flake-utils";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";        
     };
 
-    outputs = {
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = import nixpkgs { inherit system; };  
+        in {
+            devShell = pkgs.mkShell {
+                buildInputs = with pkgs; [
+                    gcc 
+                    gnumake
+                    gdb
+                ];
+            };
 
-    };
+        }
+    );
 }
